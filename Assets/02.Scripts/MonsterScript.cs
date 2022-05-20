105e2762badea4ac18d84fc4c9888bd418d5d675
jinsso2 <wlstjqkr1234@gmail.com>
2022-05-20 22:26:01 +0900

    1

--- Assets/02.Scripts/MonsterScript.cs
+++ Assets/02.Scripts/MonsterScript.cs
@@ -6,14 +6,33 @@ using UnityEngine;
 using UnityEngine.AI;
 public class MonsterScript : MonoBehaviour
 {
-    private Transform mosterTr;
+    public enum State
+    {
+        IDLE,
+        PATROL,
+        TRACE,
+        ATTACK,
+        DIE
+    }
+
+    // 몬스터의 현재 상태
+    public State state = State.IDLE;
+    // 추적 사정거리
+    public float traceDist = 10.0f;
+    // 공격 사정거리
+    public float attactDist = 2.0f;
+    // 몬스터의 사망 여부
+    public bool isDie = false;
+
+    private Transform monsterTr;
     private Transform playerTr;
     private NavMeshAgent agent;
+    private Animator anim;
 
     void Start()
     {
         // 몬스터의 Transfrom 할당
-        mosterTr = GetComponent<Transform>();
+        monsterTr = GetComponent<Transform>();
 
         // 추적 대상인 Plyaer의 태그를 찾아 Transform 할당
         playerTr = GameObject.FindWithTag("PLAYER").GetComponent<Transform>();
@@ -21,13 +40,92 @@ public class MonsterScript : MonoBehaviour
         // NavMeshAgent 컴포넌트 할당
         agent = GetComponent<NavMeshAgent>();
 
+        // Animator 컴포넌트 할당
+        anim = GetComponent<Animator>();
+
         // 추적 대상의 위치를 설정하면 바로 추적 시작
-        agent.destination = playerTr.position;
+        // agent.destination = playerTr.position;
+
+        // 몬스터의 상태를 체크하는 코루틴 함수 호출
+        StartCoroutine(CheckMonsterState());
+        // 상태에 따라 몬스터의 행동을 수행하는 코루틴 함수 호출
+        StartCoroutine(MonsterAction());
     }
 
-   
-    void Update()
+    IEnumerator CheckMonsterState()
     {
-        
+        while (!isDie)
+        {
+            // 0.3초동안 중지(대기) 하는 동안 제어권을 메시지 루프에 양보
+            yield return new WaitForSeconds(0.3f);
+
+            // 몬스터와 주인공 캐릭터 사이의 거리 측정
+            float distance = Vector3.Distance(playerTr.position, monsterTr.position);
+
+            //공격 사정거리 범위로 들어왔는지 확인
+            if(distance <= attactDist)
+            {
+                state = State.ATTACK;
+            }
+            // 추적 사정거리 범위로 들어왔는지 확인
+            else if(distance <= traceDist)
+            {
+                state = State.TRACE;
+            }
+            else
+            {
+                state = State.IDLE;
+            }
+        }
+    }
+    
+    IEnumerator MonsterAction()
+    {
+        while(!isDie){
+            switch (state)
+            {
+                // IDLE 상태
+                case State.IDLE:
+                    // 추적 중지
+                    agent.isStopped = true;
+                    // Animator의 IsTrace 변수를 false로 설정
+                    anim.SetBool("IsTrace", false);
+                    break;
+
+                // 추적 상태
+                case State.TRACE:
+                    // 추적 대상의 좌표로 이동 시작
+                    agent.SetDestination(playerTr.position);
+                    agent.isStopped = false;
+                    // Animator의 IsTrace 변수를 true로 설정
+                    anim.SetBool("IsTrace", true);
+                    break;
+
+                // 공격 상태
+                case State.ATTACK:
+                    break;
+
+                // 사망
+                case State.DIE:
+                    break;
+            }
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        // 추적 사정거리 표시
+        if(state == State.TRACE)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, traceDist);
+        }
+        // 공격 사정거리 표시
+        if(state == State.ATTACK)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, attactDist);
+        }
     }
 }
